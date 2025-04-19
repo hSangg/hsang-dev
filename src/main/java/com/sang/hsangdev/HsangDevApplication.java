0a0ca257bcb11487ac11d5d8a7f3153be32d26a2
hoai sang <hoai.sang050@gmail.com>
2025-04-19 15:31:04 +0700

    first commit

---
+++ src/main/java/com/sang/hsangdev/HsangDevApplication.java
@@ -0,0 +1,13 @@
+package com.sang.hsangdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsangDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsangDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sang/hsangdev/HsangDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sang.hsangdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsangDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
