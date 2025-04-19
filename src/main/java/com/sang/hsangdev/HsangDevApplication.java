ac0857e057456b802fa882d5048551e53df2bc82
hoai sang <hoai.sang050@gmail.com>
2025-04-19 15:49:07 +0700

    second commit

--- src/main/java/com/sang/hsangdev/HsangDevApplication.java
+++ src/main/java/com/sang/hsangdev/HsangDevApplication.java
@@ -2,9 +2,17 @@ package com.sang.hsangdev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HsangDevApplication {
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HsangDevApplication.class, args);
